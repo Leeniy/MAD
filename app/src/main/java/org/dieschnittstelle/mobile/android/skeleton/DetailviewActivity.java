15e3cc4cdc88db0ace23b8eb7ffb397aaa490573
Leeniy <marleen@lh-net.de>
2023-05-01 16:31:01 +0200

    Vorlesung 26. April

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailviewActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailviewActivity.java
@@ -1,30 +1,60 @@
 package org.dieschnittstelle.mobile.android.skeleton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class DetailviewActivity extends AppCompatActivity {
+
+    public static final int ITEM_CREATED = 10;
+
+    public static final int ITEM_EDITED = 20;
 
     private TextView itemToDoView;
     private TextView itemDescriptionView;
+
+    private FloatingActionButton fab;
+
+    private boolean createMode;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //A determine the view
+        //A.1 determine the view
         setContentView(R.layout.activity_detailview);
 
-        //B prepare initialising the view by reading out its elemnt
-        itemToDoView = findViewById(R.id.itemToDo);
+        //A.2 prepare initialising the view by reading out its elemnt
+        itemToDoView = findViewById(R.id.ToDoName);
         itemDescriptionView = findViewById(R.id.todoDescription);
+        fab = findViewById(R.id.fab);
 
-        //C bind data to the view element
+        //B bind data to the view element
         String item = getIntent().getStringExtra("item");
+        if (item != null){
+            itemToDoView.setText(item);
+        }
+        else {
+            this.createMode = true;
+        }
 
-        itemToDoView.setText(item);
+
+        //C prepare the view for user interaction
+        fab.setOnClickListener(view -> {
+            onItemSaved();
+        });
+    }
+
+    protected void onItemSaved(){
+        String item = itemToDoView.getText().toString();
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("item", item);
+        setResult(createMode ? ITEM_CREATED : ITEM_EDITED, returnIntent);
+        finish();
     }
 }
--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/OverviewActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/OverviewActivity.java
@@ -1,49 +1,121 @@
 package org.dieschnittstelle.mobile.android.skeleton;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-import org.w3c.dom.Text;
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class OverviewActivity extends AppCompatActivity {
 
-    private ViewGroup listView;
+    private ListView toDoListView;
+    private List<String> listData = new ArrayList<>(Arrays.asList("Rita", "Ron", "George"));
+    private ArrayAdapter<String> toDoListViewAdapter;
+
+    private FloatingActionButton fab;
+
+    private ActivityResultLauncher<Intent> DetailviewLauncherForEdit = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>(){
+                @Override
+                public void onActivityResult(ActivityResult result){
+                    if (result.getResultCode() == DetailviewActivity.ITEM_EDITED){
+                        String item = result.getData().getStringExtra("item");
+                        onToDoItemEdited(item);
+                    }
+                    else if (result.getResultCode() == Activity.RESULT_CANCELED){
+                        showMessage("edited cancelled!");
+                    }
+                }
+            }
+    );
+
+    private ActivityResultLauncher<Intent> detailviewLauncherForCreate = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>(){
+                @Override
+                public void onActivityResult(ActivityResult result){
+                    if (result.getResultCode() == DetailviewActivity.ITEM_CREATED){
+                        String item = result.getData().getStringExtra("item");
+                        onNewToDoItemCreated(item);
+                    }
+                }
+            }
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_overview);
-        this.listView = findViewById(R.id.listView);
+        this.toDoListView = findViewById(R.id.toDoListView);
+        this.fab = findViewById(R.id.fab);
 
-        Arrays.asList("Rita", "Ron", "George")
-                .forEach(listitem-> {
-                   TextView itemView = (TextView) getLayoutInflater().inflate(R.layout.activity_overview_listitem, null);
-                   itemView.setText(listitem);
-                   itemView.setOnClickListener(view -> {
-                       onListItemSelected(String.valueOf(((TextView)view).getText()));
-                   });
-                   this.listView.addView(itemView);
-                });
+        this.fab.setOnClickListener(view -> {
+            onCreateNewToDo();
+        });
 
+//        Arrays.asList("Rita", "Ron", "George")
+//                .forEach(listitem-> {
+//                   TextView itemView = (TextView) getLayoutInflater().inflate(R.layout.activity_overview_listitem, null);
+//                   itemView.setText(listitem);
+//                   itemView.setOnClickListener(view -> {
+//                       onListItemSelected(String.valueOf(((TextView)view).getText()));
+//                   });
+//                   this.toDoListView.addView(itemView);
+//                });
+
+        // initialise the List
+        this.toDoListViewAdapter = new ArrayAdapter<>(this, R.layout.activity_overview_listitem, listData);
+        this.toDoListView.setAdapter(this.toDoListViewAdapter);
+        this.toDoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int positionOfSelectedElement, long l) {
+                String selectedElement = toDoListViewAdapter.getItem(positionOfSelectedElement);
+                onListItemSelected(selectedElement);
+            }
+        });
     }
 
-    protected void onListItemSelected(String listitem){
+    public void onListItemSelected(String listitem){
 //        DetailviewActivity detailviewActivity = new DetailviewActivity();
 //        detailviewActivity.onCreate(null);
         Intent callDetailviewIntent = new Intent(this, DetailviewActivity.class);
         callDetailviewIntent.putExtra("item", listitem);
-        startActivity(callDetailviewIntent);
+//        startActivity(callDetailviewIntent);
+        DetailviewLauncherForEdit.launch(callDetailviewIntent);
     }
 
-    protected void showMessage(String msg){
+    public void onCreateNewToDo(){
+        detailviewLauncherForCreate.launch(new Intent(this, DetailviewActivity.class));
+    }
+
+    public void onNewToDoItemCreated(String item){
+        //showMessage("created: " + item);
+        this.toDoListViewAdapter.add(item);
+    }
+
+    public void onToDoItemEdited(String item){
+        showMessage("edited: " + item);
+    }
+
+    public void showMessage(String msg){
 //        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
         Snackbar.make(findViewById(R.id.rootView), msg, Snackbar.LENGTH_SHORT).show();
     }
